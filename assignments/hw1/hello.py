f2c9b1eb7e897b35211e5d521669bb91da8ba604
mileseriksen <eriksenma1@g.cofc.edu>
2021-09-01 16:45:11 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Miles Eriksen
+hello.py
+
+Problem: this program illustrates "hello, world!"
+
+Certification of Authenticity
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+print("hello, world!")
